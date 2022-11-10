6901d7d91397e149a9d18a3aa732f3a9374e0bf1
longy <longhanzhang999@gmail.com>
2022-11-10 23:53:48 +1030

    Update navigation bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineProject, AiFillCustomerService } from "react-icons/ai";
+import { BiUserCircle, BiBookReader } from "react-icons/bi";
 import './Nav.css'
 
 const Nav = () => {
     return (
         <nav>
             <a href="#"> <AiOutlineHome /></a>
+            <a href="#About"> <BiUserCircle /></a>
+            <a href="#Experience"> <BiBookReader /></a>
+            <a href="#Portfolio"> <AiOutlineProject /></a>
+            <a href="#Service"> <AiFillCustomerService /></a>
+            <a href="#Service"> <AiFillCustomerService /></a>
         </nav>
     )
 }
