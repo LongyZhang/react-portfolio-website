2d3e02782439466b38b318a1e5361383fc536365
longy <longhanzhang999@gmail.com>
2022-11-16 01:01:23 +1030

    Jsonify my manual data, less static code

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,128 +3,165 @@ import './Experience.css'
 import { BsFillPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
+
+    const data = [
+        {
+            type: "web development",
+            content: [
+                {
+                    id: 1,
+                    name: "HTML5",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 2,
+                    name: "CSS3 ,Sass ,React Icon ,Bootstrap",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 3,
+                    name: "React, Redux, Route",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 4,
+                    name: ".Net Core",
+                    comment: "Intermediate"
+
+                },
+
+            ]
+        },
+        {
+            type: "Gis Development",
+            content: [
+                {
+                    id: 1,
+                    name: "GE Smallworld",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 1,
+                    name: "Magik Programming",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 1,
+                    name: "ArcGis",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 1,
+                    name: "QGIS",
+                    comment: "Intermediate"
+
+                },
+            ]
+        },
+        {
+            type: "Other Techs",
+            content: [
+                {
+                    id: 1,
+                    name: "C++",
+                    comment: "Competent"
+
+                },
+                {
+                    id: 2,
+                    name: "MSSQL,MySql",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 3,
+                    name: "GIT",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 4,
+                    name: "Jira, Kanban, Rmtrack,Azure Board",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 5,
+                    name: "Python",
+                    comment: "Intermediate"
+
+                },
+                {
+                    id: 6,
+                    name: "Data Visualization",
+                    comment: "Intermediate"
+
+                },
+            ]
+        },
+        {
+            type: "Soft Skills",
+            content: [
+                {
+                    id: 1,
+                    name: "Problem solving skills",
+                    comment: "Reading documentation, Google, StackOverflow and Github Open Source"
+
+                },
+                {
+                    id: 1,
+                    name: "Efficient Communicator",
+                    comment: "Good Customer skill, Team Player"
+
+                },
+                {
+                    id: 1,
+                    name: "Great Coding style",
+                    comment: "Comment, Indentation"
+
+                },
+                {
+                    id: 1,
+                    name: "Presentation skills",
+                    comment: "Good Public Speaker"
+
+                },
+            ]
+        }
+    ]
+
     return (
         <section id='Experience' >
             <h5>The skills i have</h5>
             <h2>My Experienece</h2>
             <div className="container experience_container">
-                <div className="experience_web">
-                    <h3>Web Development</h3>
-                    <div className="experience_content">
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>HTML5</span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>CSS3 ,Sass ,React Icon ,Bootstrap </span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>JavaScript</span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>React, Redux, Route, </span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>.Net Core</span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
+                {data.map((obj) => {
+                    return <div className="experience_web">
+                        <h3>{obj.type}</h3>
+                        <div className="temp">
+                            {obj.content.map((subObj) => {
+                                return <div className="experience_content">
+                                    <article className='experience_details'>
+                                        <BsFillPatchCheckFill className='experience_icon' />
+                                        <span>{subObj.name}</span>
+                                        <small className='text_light'>{subObj.comment}</small>
+                                    </article>
+                                </div>
+
+                            })}
+                        </div>
                     </div>
-                </div>
-                <div className="experience_gis_development">
-                    <h3>GIS Development</h3>
-                    <div className="experience_content">
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>GE Smallworld</span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>Magik Programming</span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>ArcGis</span>
-                            <small className='text_light'>Experienced</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>QGis</span>
-                            <small className='text_light'>Experienced</small>
-                        </article>
-                    </div>
-                </div>
-                <div className="experience_tech">
-                    <h3>Other Techs</h3>
-                    <div className="experience_content">
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>C++</span>
-                            <small className='text_light'>Competent</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>MSSQL,MySql</span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>GIT</span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>Jira, Kanban, Rmtrack,Azure Board</span>
-                            <small className='text_light'>intermediate</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>Python</span>
-                            <small className='text_light'>Experienced</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>Data visualization</span>
-                            <small className='text_light'>Experienced</small>
-                        </article>
-                    </div>
-                </div>
-                <div className="experience_soft">
-                    <h3>Soft skills</h3>
-                    <div className="experience_content">
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>Problem solving skills</span>
-                            <small className='text_light'>Reading documentation</small>
-                            <small className='text_light'>Google,stackOverflow and Github</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>Efficient communicator</span>
-                            <small className='text_light'>Dealing customer</small>
-                            <small className='text_light'>Good teamworker</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>Great Coding style</span>
-                            <small className='text_light'>Comment, indentation</small>
-                        </article>
-                        <article className='experience_details'>
-                            <BsFillPatchCheckFill className='experience_icon' />
-                            <span>Presentation skills</span>
-                            <small className='text_light'>Experienced</small>
-                        </article>
-                    </div>
-                </div>
+
+
+
+                })}
+
             </div>
         </section>
     )
