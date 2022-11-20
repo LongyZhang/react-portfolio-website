22d7758c341d43231d7d3cc0c91d214fe2b5c4b6
longy <longhanzhang999@gmail.com>
2022-11-20 16:41:54 +1030

    Update download file name

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../images/cv.pdf'
 const CTA = () => {
     return (
         <div className='cta'>
-            <a href={CV} download className='btn'>Download CV</a>
+            <a href={CV} download="Longy_Resume.pdf" className='btn'>Download CV</a>
             <a href="#Contact" className='btn btn-primary'>Contact</a>
         </div>
     )
